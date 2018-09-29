a10fcb929f10c86026d2904659704352ac21e2d4
microsoft admin <aleyoisboix@gmail.com>
2018-09-29 18:00:25 +0200

    Update test folder

---
+++ VS_test/JS hårdkodad whitelist.js
@@ -0,0 +1,10 @@
+var sensor = 
+{
+    Id: "83860000",
+    manufacturer : "B05C",
+    version : "021B",
+    deviceType : "1C"
+};
+
+    var message = JSON.stringify(sensor);
+    console.log(message);
